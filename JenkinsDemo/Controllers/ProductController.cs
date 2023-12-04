25da1d4cc5c8b7aa3ddac8c8d772fa4934dac8eb
Bikram7852 <bikram.roy7852@gmail.com>
2023-12-04 13:07:35 +0530

    Model and controller changes

---
+++ JenkinsDemo/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JenkinsDemo.Models;
+namespace JenkinsDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel productModel = new ProductModel();
+            productModel.ProductId = 1;
+            productModel.ProductName = "Test";
+            return View();
+        }
+    }
+}
---
+++ JenkinsDemo/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JenkinsDemo.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+    }
+}
